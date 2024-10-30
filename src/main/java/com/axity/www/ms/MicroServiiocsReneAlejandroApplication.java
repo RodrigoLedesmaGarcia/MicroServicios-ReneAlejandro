ea60cca31c40e8fb12923184571fc5c09aa307a0
RodrigoLedesmaGarcia <rodrigo_ledesma_garcia@outlook.com>
2024-10-30 09:11:25 -0600

    Fist commit

---
+++ src/main/java/com/axity/www/ms/MicroServiiocsReneAlejandroApplication.java
@@ -0,0 +1,13 @@
+package com.axity.www.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiiocsReneAlejandroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiiocsReneAlejandroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/axity/www/ms/MicroServiiocsReneAlejandroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axity.www.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiiocsReneAlejandroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
